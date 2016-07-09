612cdea94ef439cb9e962db51250026105b53fef
HaiLee <duonghailee@gmail.com>
2016-07-09 21:25:11 +0300

    Create ex4.2.java

---
+++ ex4.2.java
@@ -0,0 +1,21 @@
+
+		public class Variables {
+    public static void main (String[] args) {
+       
+       int integer = -25;
+		char character = 'M';
+		String string = "This is going great!";
+		String start = "This  is the start of the text and ";
+		String end = "here is the end";
+		boolean truth = true;
+       
+        String end = "and here is the end";
+        String fullText = start + end;        
+        
+        System.out.println("Value of the integer variable is: " + integer);
+        System.out.println("Value of the character variable is: " + character);
+        System.out.println("Value of the character string variable is: " + string);
+        System.out.println("Value of the concatenated character string is: " + fullText);
+        System.out.println("Value of the truth variable is: " + truth);
+    }
+}
